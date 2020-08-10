3e8448038f70e8deb4b773fd32517dea9cd6d1d0
quarkly-dev <dev@quarkly.io>
2020-08-10 14:47:13 +0300

    set srcset & sizes

--- qapi.js
+++ qapi.js
@@ -7,6 +7,7 @@ export default {
 			"children": [
 				"5f2be7bff5f4900022453e1b",
 				"5f2be7bff5f4900022453e1d",
+				"5f312ebb5e4e25001f9646f7",
 				"5f30f1a5181d6b0017570e79"
 			]
 		},
@@ -24,6 +25,11 @@ export default {
 			"id": "5f30f1a5181d6b0017570e79",
 			"pageUrl": "all-resources",
 			"name": "all resources"
+		},
+		"5f312ebb5e4e25001f9646f7": {
+			"id": "5f312ebb5e4e25001f9646f7",
+			"pageUrl": "fixed",
+			"name": "fixed"
 		}
 	},
 	"projectType": "gatsby"
