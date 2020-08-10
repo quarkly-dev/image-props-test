f4fe38cb49eff60a9d06de7c1e3a8c5b87696727
quarkly-dev <dev@quarkly.io>
2020-08-10 14:22:20 +0300

    Some changes

---
+++ src/pages/all-resources.js
@@ -0,0 +1,119 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Image, Box, Text } from "@quarkly/widgets";
+import { Section, Override, StackItem, Stack } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Image src="https://uploads.quarkly.io/video/adaptive-image-test-amz-logo1x.png" />
+				<Image src="https://uploads.quarkly.io/video/adaptive-image-test-amz-logo2x.png" />
+				<Image src="https://uploads.quarkly.io/video/adaptive-image-test-amz-logo3x.png" />
+			</Box>
+		</Section>
+		<Box
+			box-sizing="border-box"
+			padding="24px 24px 24px 24px"
+			display="flex"
+			flex-direction="column"
+			align-items="flex-start"
+		>
+			<Image src="https://uploads.quarkly.io/video/adaptive-image-test-fullwidth-1640w.png" width="100%" />
+			<Image src="https://uploads.quarkly.io/video/adaptive-image-test-fullwidth-1280w.png" width="100%" />
+			<Image src="https://uploads.quarkly.io/video/adaptive-image-test-fullwidth-768w.png" />
+			<Image src="https://uploads.quarkly.io/video/adaptive-image-test-fullwidth-320w.png" />
+		</Box>
+		<Section background="#fff" padding="100px 0 110px 0" min-height="500px">
+			<Stack>
+				<StackItem
+					width="50%"
+					display="flex"
+					sm-width="100%"
+					sm-height="500px"
+					height="100%"
+					md-width="100%"
+					md-order="1"
+					md-height="auto"
+				>
+					<Override slot="StackItemContent" align-items="center" justify-content="center" />
+					<Image
+						src="https://uploads.quarkly.io/video/adaptive-image-test-layout-1280w.png"
+						max-width="100%"
+						height="auto"
+						object-fit="contain"
+						object-position="100%"
+						width="100%"
+						max-height="100%"
+					/>
+					{"    "}
+				</StackItem>
+				<StackItem width="50%" display="flex" sm-width="100%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" justify-content="space-between" />
+					{"        "}
+					<Text
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0px 0px 16px 0px"
+					>
+						eyebrow
+					</Text>
+					<Text
+						font="--headline1"
+						margin="0px 0px 0px 0px"
+						sm-margin="0px 0px 50px 0px"
+						flex="1 1 auto"
+						md-margin="0px 0px 33px 0px"
+					>
+						Heading
+					</Text>
+					<Text font="--base" color="--grey" margin="0px 0px 0px 0px">
+						It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+					</Text>
+					{"    "}
+				</StackItem>
+				{"    "}
+			</Stack>
+		</Section>
+		<Box
+			box-sizing="border-box"
+			padding="24px 24px 24px 24px"
+			display="flex"
+			flex-direction="column"
+			align-items="flex-start"
+		>
+			<Image
+				src="https://uploads.quarkly.io/video/adaptive-image-test-layout-1280w.png"
+				height="auto"
+				max-height="100%"
+				max-width="100%"
+				object-fit="contain"
+				object-position="100%"
+			/>
+			<Image
+				src="https://uploads.quarkly.io/video/adaptive-image-test-layout-768w.png"
+				height="auto"
+				max-height="100%"
+				max-width="100%"
+				object-fit="contain"
+				object-position="100%"
+			/>
+			<Image
+				src="https://uploads.quarkly.io/video/adaptive-image-test-layout-320w.png"
+				height="auto"
+				max-height="100%"
+				max-width="100%"
+				object-fit="contain"
+				object-position="100%"
+			/>
+		</Box>
+	</Theme>;
+});
